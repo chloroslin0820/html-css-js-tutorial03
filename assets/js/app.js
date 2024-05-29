78dcfca7c103abe7a6fe98ba19fdc4cf48a41c86
chloros <chloros.lin@gmail.com>
2024-05-30 02:59:42 +0800

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick = () => {
+
+}
